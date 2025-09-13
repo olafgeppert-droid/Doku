4d069c9bbe8f9ce0eb2e689d1382efbc661ddf07
olafgeppert-droid <olaf.geppert@googlemail.com>
2025-09-13 22:50:00 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,22 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  const env = loadEnv(mode, '.', '');
+  return {
+    // WICHTIG: Repository-Name eintragen, z.B. "/my-repo/"
+    base: '/Doku/',
+    // damit die Action das gleiche Verzeichnis hochlädt
+    build: {
+      outDir: 'out'
+    },
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    }
+  };
 });
